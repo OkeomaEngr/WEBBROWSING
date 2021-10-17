6451356a04cd09c3b3e640e1b53a1d2478aaa3e7
Okeoma Nwabueze <okeomanwabueze@trentu.ca>
2021-10-16 20:08:37 -0400

    Update WebBrowser.cs

--- WebBrowser.cs
+++ WebBrowser.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WebBrowsing
 {
-    namespace stack
+    namespace DoubleStack
     {
         //common interface for all linear data structures
         public interface IContainer<T>
@@ -20,7 +20,7 @@ namespace WebBrowsing
         {
             void Push(T item); // Place an item on the top of a stack
             void Pop();        // Remove the top item of a stack
-             T Top();           // Return the top item of a stack
+             T Top();          // Return the top item of a stack
         }
 
 
@@ -28,25 +28,24 @@ namespace WebBrowsing
         // Tabs
         // Implementation : Linear Array 
 
-        public class Tabs<T> : IStack<T> 
+        public class Tab<T> : IStack<T> 
         {
             // Data Members
             private int capacity; // Maximum capacity of the list 
             private int count;    // number of items in the list 
             private T[] A;        // Linear array of items
-            private Stack<string> S1;
-            private Stack<string> S2;
+            private Stack<string> S1; // First stack of tabs
+            private Stack<string> S2; // second stack of tabs
 
 
             //  Constructor 
-            // Create an empty list with a capacity of size 
+            // Create an empty Tab list with a capacity of size n
             // Time complexity : 0(1)
 
-            public Tabs(int n) 
+            public Tab(int n) 
             {
                 A = new T[n];
-                capacity = n;
-                count = 0;
+                size = n;
                 S1 = -1; 
                 S2 = size;        
             }
@@ -62,8 +61,7 @@ namespace WebBrowsing
                 }
                 else 
                 {
-                    Console.WriteLine (" Stack is full ");
-                    /** need a another word for - Environment.exit(1); **/
+                    Console.WriteLine (" Error, Can't add more webpages");                 
                 }
             }
 
@@ -78,12 +76,12 @@ namespace WebBrowsing
                 }
                 else
                 {
-                    Console.WriteLine("Stack is full");
-                    /** need a another word for - Environment.exit(1); **/
+                    Console.WriteLine("Error, Can't add more webpages");
                 }
             }
 
             //method to Pop an element from stack S1
+            //method to remove the webpage from the first stack
             public virtual int pop1()
              {
                 if (S1 >= 0) 
@@ -94,45 +92,81 @@ namespace WebBrowsing
                 }
                 else 
                 {
-                    Console.WriteLine("Stack empty");
-                    /** need a another word for - Environment.exit(1); **/
+                    Console.WriteLine("Error , no more webpages to remove ");
                 }
                  return 0;
             }
 
             //method to pop an element from stack S2
+            //method to remove the webpage from the second stack
             public virtual int pop2()
             {
-                if (top2 < size) 
+                if (S2 < size) 
                 {
-                    int x = arr[top2];
-                    top2++;
+                    int x = arr[S2];
+                    S2++;
                     return x;
                 }
                 else 
                 {   
-                    Console.WriteLine("Stack Underflow");
-                    /** need a another word for - Environment.exit(1); **/
+                    Console.WriteLine("Error , no more webpages to remove");                 
                 }
                 return 0;
             }
 
+            //method to retrieve a webpage from S1 
+            //returns default(T) if the stack is empty
+            public T Top1()
+            {
+                if (!Empty())
+                    return A[S1 - 1];
+                else
+                    return default(T);
+            }
+
+            //method to retrieve a webpage from S2 
+            //returns default(T) if the stack is empty
+            public T Top2()
+            {
+                if (!Empty())
+                    return A[S2 - 1];
+                else
+                    return default(T);
+            }
+
+            // MakeEmpty
+            // Resets the stack to empty
+            public void MakeEmpty()
+            {
+                S1 = default(T); // default webpage returned
+                S2 = 0;
+            }
+
+            // Size
+            // Returns the number of items in the stack
+            public int Size()
+            {   
+                return S1;
+                return S2; 
+            }
         }
+
+
         class Program
         {
         static void Main(string[] args)
             {
-                Tabs<string> S;
-                S = new Tabs<string>(5);
+                Tab<string> S;
+                S = new Tab<string>(5);
 
              
-                S.push1(5);
-                S.push2(10);
-                S.push2(15); /// need to change the numbers to letter----------- 
-                S.push1(11);
-                S.push2(7);
+                S.push1(A);
+                S.push2(B);
+                S.push2(C); /// need to change the numbers to letter----------- 
+                S.push1(D);
+                S.push2(E);
                 Console.WriteLine("Popped element from" + " stack1 is " + ts.pop1());
-                ts.push2(40);
+                ts.push2(F);
                 Console.WriteLine("Popped element from" + " stack2 is " + ts.pop2());
             }
         }
@@ -141,13 +175,13 @@ namespace WebBrowsing
    /**Browser class contains webpages**/
         class Browser
         {
-            private List<Tabs> T;
+            private List<Tab> T;
             private int currentTab;
         }
 
         //list with header node ------------------------------------ Amber do you need this ?
         public class List<T> : IList<T> 
-    {
+        {
         //data members
         private Node<T> head ; //reference to the head of the list 
         private int count;  //number of items in the list
@@ -158,21 +192,22 @@ namespace WebBrowsing
             head = new Node<T>();
             count = 0;
         }
-    }   
+        }   
 
 
-    namespace DoublyLinkedListTab
-    {
-        class Tab
+        namespace DoublyLinkedListTab
         {
-            private class Node
+            class Tab
             {
+                private class Node
+                {
+
+                }
 
             }
-
         }
-    }
 
+    }
 }
 
 
